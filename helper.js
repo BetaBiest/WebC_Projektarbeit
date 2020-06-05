31496477b372c028be0e9b21109d7884d17ced06
LeoSmith <55242246+BetaBiest@users.noreply.github.com>
2020-06-05 23:49:46 +0200

    exchanged value with placeholder

--- helper.js
+++ helper.js
@@ -2,7 +2,7 @@ function addPlayer (liItem_lastChild) {
   let numOfListItems = liItem_lastChild.parentElement.childElementCount;
   if(numOfListItems <= 4) {
     let new_liItem = document.createElement('li');
-    new_liItem.innerHTML = '<li><input type="text" name="" id="" value="Player ' + (numOfListItems - 1) + '"/></li>';
+    new_liItem.innerHTML = '<li><input type="text" name="" id="" placeholder="Player ' + (numOfListItems - 1) + '"/></li>';
     liItem_lastChild.parentElement.insertBefore(new_liItem, liItem_lastChild);
   }
 }
